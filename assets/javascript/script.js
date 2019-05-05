cb7d7165bfb072f104018a19a72c0a7c42d19641
will <wsherrill17@gmail.com>
2019-05-05 18:24:59 -0400

    revised

---
+++ assets/javascript/script.js
@@ -0,0 +1,18 @@
+var number = 15;
+
+var myNumber = prompt(" can you guess the secret number");
+var myNum = Number(myNumber);
+
+if (myNum === number) {
+    alert("you have selected right number!");
+
+}
+
+else if (myNum < number){
+    alert("Guess higher please");
+}
+else {
+    alert("Guess lower please");
+}
+
+
